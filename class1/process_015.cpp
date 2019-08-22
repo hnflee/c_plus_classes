bf75875fefaf7c1364e808a04a42d700fbf83e10
wyyzhc <26571864@qq.com>
2019-08-22 11:24:41 +0800

    提交代码

---
+++ class1/process_015.cpp
@@ -0,0 +1,26 @@
+/**
+ * 本节课主要介绍C++的函数的传值调用
+ */
+
+#include <iostream>
+#include <cstdio>
+#include <iomanip>
+
+using namespace std;
+
+/**
+ * 题目：传值调用的例子
+  */
+void swap(int a,int b){
+    int tmp=a;a=b;b=tmp;
+}
+int main(){
+
+    int c=1,d=2;
+    swap(c,d);
+    cout<<c<<' '<<d<<endl;
+
+
+    return 0;
+
+}
