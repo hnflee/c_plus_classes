d3ec8b4507db3520f198d96a0306aba54394e426
wyyhzc <26471864@qq.com>
2019-10-16 08:31:57 +0800

    排序算法=快速排序

---
+++ class2/lesson_11.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+
+using namespace std;
+/**
+ *题目：排序算法-快速排序c++代码
+ *author：wyyhzc
+ */
+
+void qsort(int a[],int l,int r){
+  int i,j,mid,p;
+  i=l;j=r;
+  mid=a[(l+r)/2];
+  
+  do{
+    while(a[i]<mid)i++;
+    while(a[j]>mid)j--;
+    if(i<=j){
+      p=a[i];
+      a[i]=a[j];
+      a[j]=p;
+      i++;j--;
+    }
+
+
+  }while(i<=j);
+
+  if(l<j)qsort(a,l,j);
+  if(i<r)qsort(a,i,r);
+
+  
+
+}
+
+int main(){
+  int n=10;
+  int a[]={20,40,32,67,40,20,89,300,400,15};
+  cout<<"排序前的数组："<<endl;
+  for(int i=0;i<n;i++){
+    cout<<a[i]<<" ";
+  }
+  cout<<endl;
+
+  qsort(a,0,10);
+
+  for(int i=0;i<n;i++){
+    cout<<a[i]<<" ";
+  }
+  cout<<endl;
+
+
+  return 0;
+}
